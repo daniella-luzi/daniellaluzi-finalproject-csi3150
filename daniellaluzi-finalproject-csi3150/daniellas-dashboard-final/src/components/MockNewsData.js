e85f7c062a2a8df64b9d479ca54b5a60498f1afe
Daniella <dluzi@oakland.edu>
2026-04-02 22:19:19 -0400

    committing again

--- daniellaluzi-finalproject-csi3150/daniellas-dashboard-final/src/components/MockNewsData.js
+++ daniellaluzi-finalproject-csi3150/daniellas-dashboard-final/src/components/MockNewsData.js
@@ -5,7 +5,7 @@ export const MockNewsData = [
 
     {
         "key": "1",
-        "urlToImage": "https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1000w,f_auto,q_auto:best/rockcms/2026-03/260322-project-hail-mary-vl-1224p-3d5596.jpg",
+        "image": "https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1000w,f_auto,q_auto:best/rockcms/2026-03/260322-project-hail-mary-vl-1224p-3d5596.jpg",
         "title": "'Project Hail Mary' flies to $54.5 million second weekend, horror reaches a saturation point",
         "description": "The Phil Lord and Chris Miller sci-fi adventure, starring Ryan Gosling, dipped only 32% after notching the best non-franchise opening weekend since 2023’s “Oppenheimer.” Amazon MGM’s yet, “Project Hail Mary” has grossed $300.8 million worldwide in two weeks.",
         "url": "https://www.nbcnews.com/pop-culture/pop-culture-news/project-hail-mary-flies-545-million-second-weekend-horror-reaches-satu-rcna265769"
@@ -13,7 +13,7 @@ export const MockNewsData = [
 
     {
         "key": "2",
-        "urlToImage": "https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1240w,f_auto,q_auto:best/rockcms/2026-02/260202-artemis-ii-moon-mn-1155-0402cd.jpg",
+        "image": "https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1240w,f_auto,q_auto:best/rockcms/2026-02/260202-artemis-ii-moon-mn-1155-0402cd.jpg",
         "title": "Countdown to Artemis II: What to know about NASA's moon mission",
         "description": "Four astronauts are set to lift off Wednesday on a journey around the moon — the first such mission in more than 50 years.",
         "url": "https://www.nbcnews.com/science/space/artemis-ii-nasa-moon-launch-time-astronauts-how-watch-what-know-rcna255627"
@@ -21,14 +21,14 @@ export const MockNewsData = [
 
     {
         "key": "3",
-        "urlToImage": "https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1000w,f_auto,q_auto:best/rockcms/2026-03/260329-UConn-Duke-NCAA-ww-1648-94795d.jpg",
+        "image": "https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1000w,f_auto,q_auto:best/rockcms/2026-03/260329-UConn-Duke-NCAA-ww-1648-94795d.jpg",
         "title": "From The Sports Desk: Inside a March Miracle",
         "description": "How UConn made its epic comeback to reach the Final Four.",
         "url": "https://www.nbcnews.com/sports/college-basketball/sports-newsletter-march-miracle-rcna265794"
     },
     {
         "key": "4",
-        "urlToImage": "https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1000w,f_auto,q_auto:best/rockcms/2026-03/260329-italy1-949-gwr-f7447b.jpg",
+        "image": "https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1000w,f_auto,q_auto:best/rockcms/2026-03/260329-italy1-949-gwr-f7447b.jpg",
         "title": "Italy used to rule the World Cup. Now it could miss a third straight.",
         "description": "The country has four World Cup trophies and made every tournament between 1958 to 2014. If it fails to beat Bosnia and Herzegovina on Tuesday, however, it could miss the event for the third straight time.",
         "url": "https://www.nbcnews.com/sports/soccer/fifa-world-cup-italy-bosnia-and-herzegovina-gennaro-gattuso-rcna265757"
